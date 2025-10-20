ebcac174054074950cb15f49fec634e38e1c4dba
iammorgoth <iammorgoth@proton.me>
2025-10-20 21:11:42 +0330

    feat: enhance number formatting to include thousands separators

--- lib/core/utils/format_number.dart
+++ lib/core/utils/format_number.dart
@@ -2,7 +2,15 @@ String doubleFormatNumber(double value) {
   double rounded = double.parse(value.toStringAsFixed(1));
 
   if (rounded == rounded.toInt()) {
-    return rounded.toInt().toString();
+    int intValue = rounded.toInt();
+    String result = intValue.toString();
+    if (intValue >= 1000 || intValue <= -1000) {
+      result = result.replaceAllMapped(
+        RegExp(r'(\d)(?=(\d{3})+(?!\d))'),
+        (Match m) => '${m[1]},',
+      );
+    }
+    return result;
   }
   return rounded.toString();
 }
@@ -11,7 +19,15 @@ String numFormatNumber(num value) {
   double rounded = double.parse(value.toStringAsFixed(1));
 
   if (rounded == rounded.toInt()) {
-    return rounded.toInt().toString();
+    int intValue = rounded.toInt();
+    String result = intValue.toString();
+    if (intValue >= 1000 || intValue <= -1000) {
+      result = result.replaceAllMapped(
+        RegExp(r'(\d)(?=(\d{3})+(?!\d))'),
+        (Match m) => '${m[1]},',
+      );
+    }
+    return result;
   }
   return rounded.toString();
 }
