c632b0a34f6bc9096a930b81f934e5077258a692
goku <goku@unboundtech.de>
2025-11-02 00:46:41 +0100

    fix over dotenv

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -38,40 +39,40 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_WEB_API_KEY'),
-    appId: String.fromEnvironment('FIREBASE_WEB_APP_ID'),
-    messagingSenderId: String.fromEnvironment('FIREBASE_PROJECT_NUMBER'),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID'),
-    authDomain: '${String.fromEnvironment('FIREBASE_PROJECT_ID')}.firebaseapp.com',
-    storageBucket: '${String.fromEnvironment('FIREBASE_PROJECT_ID')}.firebasestorage.app',
-    measurementId: String.fromEnvironment('FIREBASE_WEB_MEASUREMENT_ID'),
+  static FirebaseOptions get web => FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_WEB_API_KEY'] ?? '',
+    appId: dotenv.env['FIREBASE_WEB_APP_ID'] ?? '',
+    messagingSenderId: dotenv.env['FIREBASE_PROJECT_NUMBER'] ?? '',
+    projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
+    authDomain: '${dotenv.env['FIREBASE_PROJECT_ID']}.firebaseapp.com',
+    storageBucket: '${dotenv.env['FIREBASE_PROJECT_ID']}.firebasestorage.app',
+    measurementId: dotenv.env['FIREBASE_WEB_MEASUREMENT_ID'] ?? '',
   );
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_ANDROID_API_KEY'),
-    appId: String.fromEnvironment('FIREBASE_ANDROID_APP_ID'),
-    messagingSenderId: String.fromEnvironment('FIREBASE_PROJECT_NUMBER'),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID'),
-    storageBucket: '${String.fromEnvironment('FIREBASE_PROJECT_ID')}.firebasestorage.app',
+  static FirebaseOptions get android => FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_ANDROID_API_KEY'] ?? '',
+    appId: dotenv.env['FIREBASE_ANDROID_APP_ID'] ?? '',
+    messagingSenderId: dotenv.env['FIREBASE_PROJECT_NUMBER'] ?? '',
+    projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
+    storageBucket: '${dotenv.env['FIREBASE_PROJECT_ID']}.firebasestorage.app',
   );
 
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_IOS_API_KEY'),
-    appId: String.fromEnvironment('FIREBASE_IOS_APP_ID'),
-    messagingSenderId: String.fromEnvironment('FIREBASE_PROJECT_NUMBER'),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID'),
-    storageBucket: '${String.fromEnvironment('FIREBASE_PROJECT_ID')}.firebasestorage.app',
+  static FirebaseOptions get ios => FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_IOS_API_KEY'] ?? '',
+    appId: dotenv.env['FIREBASE_IOS_APP_ID'] ?? '',
+    messagingSenderId: dotenv.env['FIREBASE_PROJECT_NUMBER'] ?? '',
+    projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
+    storageBucket: '${dotenv.env['FIREBASE_PROJECT_ID']}.firebasestorage.app',
     iosBundleId: 'de.unboundtech.defyxvpn',
   );
 
-  static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: String.fromEnvironment('FIREBASE_WINDOWS_API_KEY'),
-    appId: String.fromEnvironment('FIREBASE_WINDOWS_APP_ID'),
-    messagingSenderId: String.fromEnvironment('FIREBASE_PROJECT_NUMBER'),
-    projectId: String.fromEnvironment('FIREBASE_PROJECT_ID'),
-    authDomain: '${String.fromEnvironment('FIREBASE_PROJECT_ID')}.firebaseapp.com',
-    storageBucket: '${String.fromEnvironment('FIREBASE_PROJECT_ID')}.firebasestorage.app',
-    measurementId: String.fromEnvironment('FIREBASE_WINDOWS_MEASUREMENT_ID'),
+  static FirebaseOptions get windows => FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_WINDOWS_API_KEY'] ?? '',
+    appId: dotenv.env['FIREBASE_WINDOWS_APP_ID'] ?? '',
+    messagingSenderId: dotenv.env['FIREBASE_PROJECT_NUMBER'] ?? '',
+    projectId: dotenv.env['FIREBASE_PROJECT_ID'] ?? '',
+    authDomain: '${dotenv.env['FIREBASE_PROJECT_ID']}.firebaseapp.com',
+    storageBucket: '${dotenv.env['FIREBASE_PROJECT_ID']}.firebasestorage.app',
+    measurementId: dotenv.env['FIREBASE_WINDOWS_MEASUREMENT_ID'] ?? '',
   );
 }
