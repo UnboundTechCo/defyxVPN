a0b0178d016da9e80e25a150eaf7b29249acccc0
iammorgoth <iammorgoth@proton.me>
2025-11-10 21:47:13 +0330

    Refactor web view implementation for improved desktop support and error handling

--- lib/shared/layout/navbar/widgets/custom_webview_screen.dart
+++ lib/shared/layout/navbar/widgets/custom_webview_screen.dart
@@ -1,11 +1,10 @@
 import 'dart:io';
 import 'package:defyx_vpn/core/theme/app_colors.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:webview_flutter_android/webview_flutter_android.dart';
-import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
 class CustomWebViewScreen extends StatefulWidget {
   final String url;
@@ -22,38 +21,20 @@ class CustomWebViewScreen extends StatefulWidget {
 }
 
 class _CustomWebViewScreenState extends State<CustomWebViewScreen> {
-  WebViewController? _controller;
+  late final WebViewController _controller;
   bool _isLoading = true;
-  bool _isUnsupportedPlatform = false;
 
   @override
   void initState() {
     super.initState();
-    _initializeWebView();
-  }
 
-  void _initializeWebView() {
-    if (Platform.isWindows || Platform.isLinux) {
-      setState(() {
-        _isUnsupportedPlatform = true;
-        _isLoading = false;
-      });
+    // Check if running on desktop platform
+    if (!kIsWeb && (Platform.isWindows || Platform.isLinux)) {
       _openInBrowser();
       return;
     }
 
-    late final PlatformWebViewControllerCreationParams params;
-    if (Platform.isAndroid) {
-      params = AndroidWebViewControllerCreationParams();
-    } else if (Platform.isIOS || Platform.isMacOS) {
-      params = WebKitWebViewControllerCreationParams(
-        allowsInlineMediaPlayback: true,
-      );
-    } else {
-      params = const PlatformWebViewControllerCreationParams();
-    }
-
-    _controller = WebViewController.fromPlatformCreationParams(params)
+    _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setNavigationDelegate(
         NavigationDelegate(
@@ -70,26 +51,24 @@ class _CustomWebViewScreenState extends State<CustomWebViewScreen> {
         ),
       )
       ..loadRequest(Uri.parse(widget.url));
-
-    if (_controller?.platform is AndroidWebViewController) {
-      AndroidWebViewController.enableDebugging(true);
-      (_controller!.platform as AndroidWebViewController)
-          .setMediaPlaybackRequiresUserGesture(false);
-    }
   }
 
   Future<void> _openInBrowser() async {
     final uri = Uri.parse(widget.url);
-    if (await canLaunchUrl(uri)) {
-      await launchUrl(uri, mode: LaunchMode.externalApplication);
-      if (mounted) {
-        Navigator.of(context).pop();
+    try {
+      if (await canLaunchUrl(uri)) {
+        await launchUrl(uri, mode: LaunchMode.externalApplication);
+      } else {
+        if (mounted) {
+          debugPrint('Could not launch URL: ${widget.url}');
+        }
       }
-    } else {
+    } catch (e) {
       if (mounted) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Could not open ${widget.url}')),
-        );
+        debugPrint('Error launching URL: $e');
+      }
+    } finally {
+      if (mounted) {
         Navigator.of(context).pop();
       }
     }
@@ -97,7 +76,8 @@ class _CustomWebViewScreenState extends State<CustomWebViewScreen> {
 
   @override
   Widget build(BuildContext context) {
-    if (_isUnsupportedPlatform) {
+    // For desktop platforms, show loading while opening browser
+    if (!kIsWeb && (Platform.isWindows || Platform.isLinux)) {
       return Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
@@ -157,21 +137,17 @@ class _CustomWebViewScreenState extends State<CustomWebViewScreen> {
           ),
         ),
       ),
-      body: _controller == null
-          ? Center(
-              child: CircularProgressIndicator(color: AppColors.primary),
-            )
-          : Stack(
-              children: [
-                WebViewWidget(controller: _controller!),
-                if (_isLoading)
-                  Center(
-                    child: CircularProgressIndicator(
-                      color: AppColors.primary,
-                    ),
-                  ),
-              ],
+      body: Stack(
+        children: [
+          WebViewWidget(controller: _controller),
+          if (_isLoading)
+            Center(
+              child: CircularProgressIndicator(
+                color: AppColors.primary,
+              ),
             ),
+        ],
+      ),
     );
   }
 }
